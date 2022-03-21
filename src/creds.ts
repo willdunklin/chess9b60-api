1a904579c23e42922a1a39826f83c72d1c9d21fb
willdunklin <dunkliw@clarkson.edu>
2022-03-20 22:38:50 -0400

    fix secret

--- src/creds.ts
+++ src/creds.ts
@@ -1,8 +1,7 @@
-import { accessKey, secretKey } from "./secret";
 import { Credentials } from "@aws-sdk/types";
 
-const access = process.env.NODE_ENV === "production" ? process.env.REACT_APP_AWS_KEY_ID : accessKey;
-const secret = process.env.NODE_ENV === "production" ? process.env.REACT_APP_AWS_SECRET_KEY : secretKey;
+const access = process.env.NODE_ENV === "production" ? process.env.REACT_APP_AWS_KEY_ID : require('./secret').accessKey;
+const secret = process.env.NODE_ENV === "production" ? process.env.REACT_APP_AWS_SECRET_KEY : require('./secret').secretKey;
 
 export const creds: Credentials = { 
     accessKeyId: access || "invalid",
